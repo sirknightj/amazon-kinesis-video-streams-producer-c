d54c2723194f3a7b95b804e577ab6f620836a6fd
sirknightj <jggunawa@amazon.com>
2025-02-20 12:42:28 -0800

    Debugging

--- tst/scripts/test_cmake_flags.sh
+++ tst/scripts/test_cmake_flags.sh
@@ -36,7 +36,7 @@ for combination in "${COMBINATIONS[@]}"; do
   mkdir build
 
   # Run only the CMake configuration with the current combination
-  cmake -B build $FLAGS -DBUILD_DEPENDENCIES=OFF > /dev/null 2>&1
+  cmake -B build $FLAGS -DBUILD_DEPENDENCIES=OFF
   EXIT_CODE=$?
 
   # Check if the exit code matches the expected result
